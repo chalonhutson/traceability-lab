2c4294ef3911d8fc3c7140169bc91e50bb7a5213
chalonhutson <chalonhutson@gmail.com>
2021-08-17 13:56:43 -0600

    dksjfldksj

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const cors = require("cors");
-const ctrl = require("./controller.js");
+// const ctrl = require("./controller.js");
 
 const path = require("path");
 
@@ -10,28 +10,25 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 
-const port = process.env.PORT || 5000;
 
 app.use(express.static("client"));
-// app.use(express.static("resources"));
 
-// include and initialize the rollbar library with your access token
 let Rollbar = require("rollbar");
 let rollbar = new Rollbar({
-  accessToken: '3d7f98e1119d4e658467f6bd7f7b33d2',
-  captureUncaught: true,
-  captureUnhandledRejections: true
+    accessToken: '3d7f98e1119d4e658467f6bd7f7b33d2',
+    captureUncaught: true,
+    captureUnhandledRejections: true
 });
 
-// record a generic message and send it to Rollbar
 
-rollbar.log("Homepsfsdfsadfasdfasdfsafdage hit!");
 
 app.get("/", (req, res) => {
-    rollbar.log("Homepsfsdfsadfasdfasdfsafdage hit!");
+    console.log("testing 232342")
+    rollbar.log("Test 1223123")
     res.sendFile(path.join(__dirname, "../client/index.html"))
-})
+});
 
+const port = process.env.PORT || 5050;
 
 // app.get("/api/getPass/:password", ctrl.passEncrypt)
 // app.post("/api/login", ctrl.login)
